fc75fc2e90a829e329cc48b60729c6acd6d4a1b4
Nr0ker <125687629+Nr0ker@users.noreply.github.com>
2023-08-30 14:16:42 +0200

    add db

--- config.py
+++ config.py
@@ -3,5 +3,7 @@ import os
 
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'u-will-never-guess'
-    SQLALCHEMY_DATABASE_URI = 'mysql://root:@localhost:3306/flask_database'
+    SQLALCHEMY_DATABASE_URI = 'mysql://root:@localhost:3306/project--2'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+
