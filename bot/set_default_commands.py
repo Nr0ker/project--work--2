369138458ff11ad21a1addff4b0e6bf163739d69
Nr0ker <125687629+Nr0ker@users.noreply.github.com>
2023-09-01 17:07:56 +0200

    test admin

--- bot/set_default_commands.py
+++ bot/set_default_commands.py
@@ -5,7 +5,17 @@ async def set_default_commands(dp):
     await dp.bot.set_my_commands(
         [
             types.BotCommand("start", "Запустити бота"),
-            types.BotCommand("help", "Отримати допомогу")
+            types.BotCommand("help", "Отримати допомогу"),
+            types.BotCommand("send_anketa", "Надіслати анкету"),
+            types.BotCommand("confirm", "Зробити анкету"),
+            types.BotCommand("do_my_resume", "Почати робити резюме"),
+            types.BotCommand("info", "Інформація про бота"),
+            types.BotCommand("", "Дізнатися ФІО"),
+            types.BotCommand("info", "Інформація про бота"),
+            types.BotCommand("info", "Інформація про бота"),
+            types.BotCommand("info", "Інформація про бота"),
+            types.BotCommand("info", "Інформація про бота"),
+            types.BotCommand("info", "Інформація про бота"),
 
         ]
     )
