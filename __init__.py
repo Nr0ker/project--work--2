e274e35d76b71592eeec5e926585106b19df6d6b
Zaldrik <suhariksgames@gmail.com>
2023-09-01 14:28:40 +0300

    DIMAA IS MY NIGGA <3

---
+++ __init__.py
@@ -0,0 +1,30 @@
+from aiogram.utils import executor
+from bot.data.config import dp, bot
+from bot.set_default_commands import set_default_commands
+from aiogram import types
+from bot.keyboards import kb_cities
+from bot.statesfile import StepStates
+
+list_for_filters = []
+
+
+@dp.message_handler(commands="start")
+async def start(message: types.Message):
+    await bot.send_message(text="Вітаємо, виберіть місто в якому ви хочете знайти роботу", chat_id=message.from_user.id, reply_markup=kb_cities)
+    await StepStates.sity.set()
+
+
+
+
+@dp.message_handler(commands="help")
+async def help(message: types.Message):
+    await bot.send_message(text="Цей бот допоможе з вибором роботи на сайті robota.ua;"
+                                "Команди: /start"
+                                "/help", chat_id=message.from_user.id)
+
+
+async def on_startup(dispatcher):
+    await set_default_commands(dispatcher)
+
+
+executor.start_polling(dp, on_startup=on_startup, skip_updates=True)
--- bot/keyboards.py
+++ bot/keyboards.py
@@ -1,30 +1,14 @@
 from aiogram import types
 
-kb_cities = types.InlineKeyboardMarkup(
+kb_resume = types.InlineKeyboardMarkup(
     inline_keyboard=[
-        [types.InlineKeyboardButton(text='Київ', callback_data='Kyiv_callback')],
-        [types.InlineKeyboardButton(text='Дніпро', callback_data='dnypro_callback')],
-        [types.InlineKeyboardButton(text='Харків', callback_data='charkiv_callback')],
-        [types.InlineKeyboardButton(text='Запоріжжя', callback_data='zapori_callback')],
-        [types.InlineKeyboardButton(text='Одеса', callback_data='odesa_callback')],
-        [types.InlineKeyboardButton(text='Львів', callback_data='lviv_callback')],
-        [types.InlineKeyboardButton(text='Пошук по всій Україні', callback_data='all_callback')]
-
-    ]
-)
-
-kb_rayon = types.InlineKeyboardMarkup(
-    inline_keyboard=[
-        [types.InlineKeyboardButton(text=' Голосіївський ', callback_data='golos_callback')],
-        [types.InlineKeyboardButton(text='Дарницький', callback_data='darn_callback')],
-        [types.InlineKeyboardButton(text='Деснянський', callback_data='desn_callback')],
-        [types.InlineKeyboardButton(text='Дніпровський', callback_data='dnipro_callback')],
-        [types.InlineKeyboardButton(text='Оболонський', callback_data='obolon_callback')],
-        [types.InlineKeyboardButton(text='Печерський', callback_data='pechersk_callback')],
-        [types.InlineKeyboardButton(text='Подільський', callback_data='podil_callback')],
-        [types.InlineKeyboardButton(text='Святошинський', callback_data='svyatosh_callback')],
-        [types.InlineKeyboardButton(text="Солом'янський", callback_data='solom_callback')],
-        [types.InlineKeyboardButton(text='Шевченківський', callback_data='shevch_callback')]
-
+        [types.InlineKeyboardButton(text="Прізвище Ім`я По-батькові", callback_data='PIB')],
+        [types.InlineKeyboardButton(text="Дата народження", callback_data='birthdate')],
+        [types.InlineKeyboardButton(text="Адреса прописки", callback_data='adress')],
+        [types.InlineKeyboardButton(text="Номер телефону", callback_data='phone_num')],
+        [types.InlineKeyboardButton(text="Інші способи зв'язку", callback_data='communication')],
+        [types.InlineKeyboardButton(text="Електронна адреса", callback_data='email')],
+        [types.InlineKeyboardButton(text="Освіта", callback_data='osvita')],
+        [types.InlineKeyboardButton(text="Додатково", callback_data='add')]
     ]
 )
