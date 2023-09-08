3cf37cb24e0a8b8221cdaced34c56bb25c49de4a
Nr0ker <125687629+Nr0ker@users.noreply.github.com>
2023-09-08 12:02:47 +0200

    change all

--- database.py
+++ database.py
@@ -4,22 +4,20 @@ db = mysql.connector.connect(
     host="localhost",
     user="root",
     password="",
-    database="my_resume_db"
+    database="project--2"
 )
 
 cursor = db.cursor()
 cursor.execute("""
-    CREATE TABLE IF NOT EXISTS resumes (
-        id INT AUTO_INCREMENT PRIMARY KEY,
-        pib VARCHAR(255),
-        birthdate DATE,
-        address VARCHAR(255),
-        phone_num VARCHAR(255),
-        communication TEXT,
-        email VARCHAR(255), 
-        osvita VARCHAR(255),
-        add_info TEXT
-    )
+        CREATE TABLE IF NOT EXISTS resumes (
+            id INT AUTO_INCREMENT PRIMARY KEY,
+            pib VARCHAR(255),
+            birthdate DATE,
+            phone_num VARCHAR(255),
+            email VARCHAR(255), 
+            osvita VARCHAR(255),
+            user_id VARCHAR(255)
+        )
 """)
 db.commit()
 cursor.close()
